3d57d3fe7418237857874c50955db23720c0b6e9
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-08-26 20:09:39 -0300

    post and get transactions with balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce((transactionAccumulator: Balance, currentTransaction: Transaction) => {
+      switch (currentTransaction.type) {
+        case 'income':
+          transactionAccumulator.income += currentTransaction.value;
+          break;
+        case 'outcome':
+          transactionAccumulator.outcome += currentTransaction.value;
+          break;
+        default:
+          break;
+      }
+
+      return transactionAccumulator;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    });
+
+    const total = income - outcome;
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
